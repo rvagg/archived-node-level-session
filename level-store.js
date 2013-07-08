4069d1ef4f2b1cb25d3d05df58350e523c4b4af2
Rod Vagg <rod@vagg.org>
2013-07-08 15:03:44 +1000

    reformat and update for latest sublevel & ttl

--- level-store.js
+++ level-store.js
@@ -1,127 +1,126 @@
 const genericSession = require('generic-session')
-    , levelup        = require('levelup')
+    , levelup        = require('level')
     , ttl            = require('level-ttl')
+    , sublevel       = require('level-sublevel')
     , xtend          = require('xtend')
 
     , SEP_CHAR       = '|' // session key is Base64 and has a prefix 'session:' so | avoids dups
     , LUP_OPTIONS    = { keyEncoding: 'utf8', valueEncoding: 'utf8' }
 
-  , dbkey      = function (id, key) {
-      return id + SEP_CHAR + key
-    }
+function dbkey (id, key) {
+  return id + SEP_CHAR + key
+}
 
-var LevelStore = {
-        get: function (id, key, expire, callback) {
-          // brutish but it updates ttl on all entries
-          this.getAll(id, expire, function (err, data) {
-            if (err) return callback(err)
-            callback(null, data[key])
-          })
-        }
+function LevelStore (options) {
+  if (!(this instanceof LevelStore))
+    return new LevelStore(options)
 
-      , getAll: function (id, expire, callback) {
-          var ret   = {}
-            , batch = []
+  if (typeof options == 'string')
+    options = { location: options }
 
-          this._forEach(
-              id
-            , function (key, value) {
-                ret[key] = JSON.parse(value)
-                batch.push({ type: 'put', key: dbkey(id, key), value: value })
-              }
-            , function () {
-                // put them back for the ttl update
-                this._db.batch(
-                    batch
-                  , xtend(LUP_OPTIONS, { ttl: expire })
-                  , function (err) {
-                      if (err) return callback(err)
-                      callback(null, ret)
-                    }
-                )
-              }.bind(this)
-          )
-        }
+  if (!options || (typeof options.location != 'string' && typeof options.db != 'object'))
+    throw new Error('You must provide a `location` option for your LevelDB store or a LevelUP `db` instance')
 
-      , set: function (id, key, value, expire, callback) {
-          this.extend(id, expire)
-          this._db.put(
-              dbkey(id, key)
-            , JSON.stringify(value)
-            , xtend(LUP_OPTIONS, { ttl: expire })
-            , callback
-          )
-        }
+  this._db = options.db || levelup(options.location, {
+      errorIfExists   : false
+    , createIfMissing : true
+    , keyEncoding     : 'utf8'
+    , valueEncoding   : 'utf8'
+  })
 
-      , extend: function (id, expire) {
-          this.getAll(id, expire, function () {})
-        }
+  this._db = sublevel(this._db)
+  this._db = ttl(this._db)
+  this._sessionDb = this._db.sublevel('session')
+}
 
-      , del: function (id, key, expire, callback) {
-          this.extend(id, expire)
-          this._db.del(
-              dbkey(id, key)
-            , LUP_OPTIONS
-            , function (err) {
-                if (err) return callback(err)
-                this.extend(id, expire)
-                callback()
-              }.bind(this)
-          )
-        }
+LevelStore.prototype.get = function (id, key, expire, callback) {
+  // brutish but it updates ttl on all entries
+  this.getAll(id, expire, function (err, data) {
+    if (err) return callback(err)
+    callback(null, data[key])
+  })
+}
 
-      , delAll: function (id, callback) {
-          var batch = []
-          this._forEach(
-              id
-            , function (key) {
-                batch.push({ type: 'del', key: dbkey(id, key) })
-              }
-            , function () {
-                this._db.batch(batch, LUP_OPTIONS, callback)
-              }.bind(this)
-          )
-        }
+LevelStore.prototype.getAll = function (id, expire, callback) {
+  var ret   = {}
+    , keys = []
 
-      , close: function (callback) {
-          this._db.close(callback)
-        }
+  this._forEach(
+      id
+    , function (key, value) {
+        ret[key] = JSON.parse(value)
+        keys.push(dbkey(id, key))
+      }
+    , function () {
+        // update the ttl value for each key
+        this._db.ttl(
+            keys
+          , expire
+          , function (err) {
+              if (err) return callback(err)
+              callback(null, ret)
+            }
+        )
+      }.bind(this)
+  )
+}
 
-      , _forEach: function (id, fn, callback) {
-          var rs = this._db.readStream(xtend(LUP_OPTIONS, { start: id }))
+LevelStore.prototype.set = function (id, key, value, expire, callback) {
+  this.extend(id, expire, function () {
+    this._sessionDb.put(
+        dbkey(id, key)
+      , JSON.stringify(value)
+      , xtend(LUP_OPTIONS, { ttl: expire })
+      , callback
+    )
+  }.bind(this))
+}
 
-          rs.on('data', function (data) {
-              var lkey = data.key.split(SEP_CHAR)
-              if (lkey[0] == id)
-                fn(lkey[1], data.value)
-              else if (lkey.length != 2 || lkey[0] > id)
-                rs.destroy()
-            })
-            .on('end', callback)
-        }
-    }
+LevelStore.prototype.extend = function (id, expire, callback) {
+  this.getAll(id, expire, function (err) { callback && callback(err) })
+}
 
-  , create = function (options) {
-      if (typeof options == 'string')
-        options = { location: options }
+LevelStore.prototype.del = function (id, key, expire, callback) {
+  this.extend(id, expire, function () {
+    this._sessionDb.del(
+        dbkey(id, key)
+      , LUP_OPTIONS
+      , function (err) {
+          if (err) return callback(err)
+          this.extend(id, expire, callback)
+        }.bind(this)
+    )
+  }.bind(this))
+}
 
-      if (!options || (typeof options.location != 'string' && typeof options.db != 'object'))
-        throw new Error('You must provide a `location` option for your LevelDB store or a LevelUP `db` instance')
+LevelStore.prototype.delAll = function (id, callback) {
+  var batch = []
+  this._forEach(
+      id
+    , function (key) {
+        batch.push({ type: 'del', key: dbkey(id, key) })
+      }
+    , function () {
+        this._sessionDb.batch(batch, LUP_OPTIONS, callback)
+      }.bind(this)
+  )
+}
 
-      var db = options.db || levelup(options.location, {
-              errorIfExists   : false
-            , createIfMissing : true
-            , keyEncoding     : 'utf8'
-            , valueEncoding   : 'utf8'
-          })
-        , close
+LevelStore.prototype.close = function (callback) {
+  this._db.close(callback)
+}
 
-      db = ttl(db)
-      close = db.close.bind(db)
-      db = db.sublevel('session')
-      db.close = close // close() unimplemented in level-sublevel
+LevelStore.prototype._forEach = function (id, fn, callback) {
+  var rs = this._sessionDb.readStream(xtend(LUP_OPTIONS, { start: id }))
 
-      return Object.create(LevelStore, { _db: { value: db } })
-    }
+  rs.on('data', function (data) {
+      var lkey = data.key.split(SEP_CHAR)
+      if (lkey[0] == id)
+        fn(lkey[1], data.value)
+      else if (lkey.length != 2 || lkey[0] > id)
+        rs.destroy()
+    })
+    .on('end', callback)
+}
 
-module.exports = create
+module.exports = LevelStore
